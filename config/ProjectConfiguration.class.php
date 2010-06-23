bfdf1f75dd7a81d4e793de8918902f3a69a2ab57
Christoph Herbst <christoph.p.herbst@gmx.de>
2010-06-23 14:49:12 +0200

    Initial commit of openZIM

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/vendor/symfony-1.4.5/lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+  }
+}
