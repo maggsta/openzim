4482de112c89a17d628e63990ef51319c112e960
www <www@ultramobile.(none)>
2010-06-25 21:09:11 +0200

    User mngmnt.

--- data/sql/schema.sql
+++ data/sql/schema.sql
@@ -2,3 +2,11 @@ CREATE TABLE bild (id INTEGER PRIMARY KEY AUTOINCREMENT, path VARCHAR(255) NOT N
 CREATE TABLE stunde (id INTEGER PRIMARY KEY AUTOINCREMENT, description VARCHAR(255) NOT NULL, zim_id INTEGER NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL);
 CREATE TABLE zim (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL);
 CREATE TABLE zim_anlage (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255) NOT NULL UNIQUE, zeit INTEGER, ziel VARCHAR(255), methode VARCHAR(255), material VARCHAR(1000), tip VARCHAR(255), kurzinhalt VARCHAR(1000), inhalt VARCHAR(10000), rolle_tm VARCHAR(255), bild_id INTEGER, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL);
+CREATE TABLE sf_guard_group (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255) UNIQUE, description VARCHAR(1000), created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL);
+CREATE TABLE sf_guard_group_permission (group_id INTEGER, permission_id INTEGER, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(group_id, permission_id));
+CREATE TABLE sf_guard_permission (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255) UNIQUE, description VARCHAR(1000), created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL);
+CREATE TABLE sf_guard_remember_key (id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, remember_key VARCHAR(32), ip_address VARCHAR(50), created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL);
+CREATE TABLE sf_guard_user (id INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR(128) NOT NULL UNIQUE, algorithm VARCHAR(128) DEFAULT 'sha1' NOT NULL, salt VARCHAR(128), password VARCHAR(128), is_active INTEGER DEFAULT '1', is_super_admin INTEGER DEFAULT '0', last_login DATETIME, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL);
+CREATE TABLE sf_guard_user_group (user_id INTEGER, group_id INTEGER, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(user_id, group_id));
+CREATE TABLE sf_guard_user_permission (user_id INTEGER, permission_id INTEGER, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(user_id, permission_id));
+CREATE INDEX is_active_idx_idx ON sf_guard_user (is_active);
