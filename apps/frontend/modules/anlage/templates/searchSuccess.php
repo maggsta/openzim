dbed5189bba430ac2c82be7bab3eedacc5e92a48
www <www@ultramobile.(none)>
2010-06-29 12:25:54 +0200

    searchSuccess fixed

--- apps/frontend/modules/anlage/templates/searchSuccess.php
+++ apps/frontend/modules/anlage/templates/searchSuccess.php
@@ -1,3 +1,17 @@
-<div id="anlagen">
-  <?php include_partial('anlage/list', array('anlagen' => $anlagen)) ?>
-</div>
+  <div class="search">
+        <h2>Anlage Suchen/Filtern:</h2>
+        <form action="<?php echo url_for('anlage_search') ?>" method="get">
+                <input type="text" name="query" value="<?php echo $sf_request->getParameter('query') ?>" id="search_keywords" />
+                <input type="submit" value="GO" />
+                <img id="loader" src="/images/loader.gif" style="vertical-align: middle; display: none" />
+                <div class="help">
+                        Suche nach Keywords (Name, Ziel, Inhalt, ...)
+                </div>
+        </form>
+      </div>
+
+<h1>Anlagen</h1>
+
+<?php include_partial('anlage/list', array('anlagen' => $anlagen)) ?>
+
+<a href="<?php echo url_for('anlage/new') ?>">New</a>
