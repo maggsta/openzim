bd76270839f39a7288ab3d1edac7419c8caa768c
Christoph Herbst <christoph.p.herbst@gmx.de>
2010-06-29 13:17:25 +0200

    frontend/Anlage: DRY

---
+++ apps/frontend/modules/anlage/templates/_search.php
@@ -0,0 +1,12 @@
+<div class="search">
+   <h2>Anlage Suchen/Filtern:</h2>
+   <form action="<?php echo url_for('anlage_search') ?>" method="get">
+                <input type="text" name="query" value="<?php echo $sf_request->getParameter('query') ?>" id="search_keywords" />
+                <input type="submit" value="GO" />
+                <img id="loader" src="/images/loader.gif" style="vertical-align: middle; display: none" />
+                <div class="help">
+                        Suche nach Keywords (Name, Ziel, Inhalt, ...)
+                </div>
+   </form>
+</div>
+
