68f0386bbf4ed39aca0d5db1d066f750f011d19b
Christoph Herbst <christoph.p.herbst@gmx.de>
2010-06-30 17:45:07 +0200

    frontend/zim: do not show unneded properties

--- apps/frontend/modules/zim/templates/showSuccess.php
+++ apps/frontend/modules/zim/templates/showSuccess.php
@@ -1,20 +1,8 @@
 <table>
   <tbody>
     <tr>
-      <th>Id:</th>
-      <td><?php echo $zim->getId() ?></td>
-    </tr>
-    <tr>
       <th>Name:</th>
       <td><?php echo $zim->getName() ?></td>
-    </tr>
-    <tr>
-      <th>Created at:</th>
-      <td><?php echo $zim->getCreatedAt() ?></td>
-    </tr>
-    <tr>
-      <th>Updated at:</th>
-      <td><?php echo $zim->getUpdatedAt() ?></td>
     </tr>
   </tbody>
 </table>
