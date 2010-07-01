db207f222924fc480874ef67e72b130cd0381093
Christoph Herbst <christoph.p.herbst@gmx.de>
2010-07-01 11:39:01 +0200

    Anlage/form: added export link

--- apps/frontend/modules/anlage/templates/_form.php
+++ apps/frontend/modules/anlage/templates/_form.php
@@ -10,6 +10,7 @@
       <tr>
         <td colspan="2">
           &nbsp;<a href="<?php echo url_for('anlage/index') ?>">Back to list</a>
+          &nbsp;<a href="<?php echo url_for('anlage/export?id='.$form->getObject()->getId()) ?>">Export</a>
           <?php if (!$form->getObject()->isNew()): ?>
             &nbsp;<?php echo link_to('Delete', 'anlage/delete?id='.$form->getObject()->getId(), array('method' => 'delete', 'confirm' => 'Are you sure?')) ?>
           <?php endif; ?>
