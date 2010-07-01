6d1bae3b9fc35e03b57d829f26a140e2ffa86b32
Christoph Herbst <christoph.p.herbst@gmx.de>
2010-07-01 12:00:09 +0200

    htmlConverter: refactored, DRY

--- lib/htmlconverter/library/htmlConverter.php
+++ lib/htmlconverter/library/htmlConverter.php
@@ -5,61 +5,31 @@ class htmlConverter {
 		
 	}	
 
-	public function replaceDoubleQuotes($text) {
-
-                return str_replace('&ldquo;', '“', str_replace('&bdquo;', '„', $text));
-
-        }
-
-        public function replaceUmlaute($text) {
-
-                $text = str_replace('&uuml;', 'ü', $text);
-                $text = str_replace('&ouml;', 'ö', $text);
-                $text = str_replace('&auml;', 'ä', $text);
-                $text = str_replace('&szlig;', 'ß', $text);
-                $text = str_replace('&nbsp;', ' ', $text);
-		return $text;
-
-        }
-
-	public function convertBold($text) {
-	
-		$text = str_replace('<strong>', '<text:span text:style-name="T5">', $text);
-		$text = str_replace('</strong>', '</text:span>', $text);
-		return $text;
-	}
-
-	public function convertItalic($text) {
-
-                $text = str_replace('<em>', '<text:span text:style-name="T1">', $text);
-                $text = str_replace('</em>', '</text:span>', $text);
-		return $text;
-        
-	}
-
-	public function convertParagraph($text) {
-
-                $text = str_replace('<p>', '', $text);
-                $text = str_replace('</p>', "\n", $text);
+	public function convertFromArray($array,$text) {
+		foreach( $array as $key => $value )
+                	$text = str_replace($key, $value, $text);
                 return $text;
-
-        }
-
-	public function convertOuterList($text) {
-
-                $text = str_replace('<li>', '<text:list><text:list-item>', $text);
-                $text = str_replace('</li>', '</text:list-item></text:list>', $text);
-                return $text;
-
         }
 
 	public function getODF($text) {
+                $convertArray = array( 
+                  '&ldquo;' => '“', 
+		  '&bdquo;' => '„', 
+	          '&uuml;'  => 'ü', 
+                  '&ouml;'  => 'ö', 
+                  '&auml;'  => 'ä', 
+                  '&szlig;' => 'ß', 
+                  '&nbsp;'  => ' ', 
+		  '<strong>'  => '<text:span text:style-name="T5">',
+		  '</strong>' => '</text:span>',
+                  '<em>'    => '<text:span text:style-name="T1">',
+                  '</em>'   => '</text:span>',
+//	   	  '<li>' => '<text:list><text:list-item>',
+//                '</li>' => '</text:list-item></text:list>',
+                  '<p>'     => '', 
+                  '</p>'    => "\n");
 		$result = strip_tags($text, '<strong><em><p>');
-		$result = $this->replaceDoubleQuotes($result);
-		$result = $this->replaceUmlaute($result);
-		$result = $this->convertBold($result);	
-		$result = $this->convertItalic($result);
-		$result = $this->convertParagraph($result);
+                $result = $this->convertFromArray($convertArray,$result);
 		return $result;
 	}
 
