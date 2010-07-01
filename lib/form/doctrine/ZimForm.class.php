b647601669ce343149f44eb665c1cad0678eafcb
Christoph Herbst <christoph.p.herbst@gmx.de>
2010-07-01 23:00:55 +0200

    ZimForm: do not require anlagen

--- lib/form/doctrine/ZimForm.class.php
+++ lib/form/doctrine/ZimForm.class.php
@@ -24,7 +24,9 @@ class ZimForm extends BaseZimForm
 
 	$this->validatorSchema['Anlagen'] = 
 		new sfValidatorDoctrineChoice(array(
-			'model' => $this->getRelatedModelName('Anlagen')));
+			'model' => $this->getRelatedModelName('Anlagen'),
+			'required' => false
+			));
 		
   }
 }
