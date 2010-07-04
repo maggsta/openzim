e2ca9cbe69dc0793fd9dd6d85c2a989676c6dd72
Christoph Herbst <christoph.p.herbst@gmx.de>
2010-07-04 19:36:16 +0200

    Zim: Tidy up show template

--- apps/frontend/modules/zim/templates/showSuccess.php
+++ apps/frontend/modules/zim/templates/showSuccess.php
@@ -1,32 +1,34 @@
+<h1><?php echo $zim->getName() ?></h1>
 <table>
-  <tbody>
+  <thead>
     <tr>
-      <th>Name:</th>
-      <td><?php echo $zim->getName() ?></td>
+      <th>Nr.</th>
+      <th>Zeit</th>
+      <th>Ziel</th>
+      <th>Inhalt</th>
+      <th>Methode</th>
+      <th>Rolle TM</th>
+      <th>Anlagen/Hilfsmittel</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach ($zim->getStunden() as $stunde): ?>
+    <tr>
+       <td colspan="7"><?php echo $stunde ?></td>
     </tr>
     <tr>
-      <th>Stunden:</th>
-	<table>
-	  <tbody>
-      <?php foreach ($zim->getStunden() as $stunde): ?>
-	    <tr>
-              <td><?php echo $stunde ?></td>
-    	    </tr>
-	    <tr>
-	      <th>Anlagen:</th>
-		<table>
-		  <tbody>
-	      <?php foreach ($stunde->getAnlagen() as $anlage): ?>
-		    <tr>
-	              <td><?php echo $anlage ?></td>
-	    	    </tr>	
-	      <?php endforeach; ?>
-	  	  </tbody>
-		</table>
-	    </tr>
-      <?php endforeach; ?>
-  	  </tbody>
-	</table>
+     <?php foreach ($stunde->getAnlagen() as $anlage): ?>
+    <tr>
+       <td><?php echo $anlage->getName() ?></td>
+       <td><?php echo $anlage->getZeit() ?></td>
+       <td><?php echo $anlage->getZiel() ?></td>
+       <td><?php echo $anlage->getKurzinhalt() ?></td>
+       <td><?php echo $anlage->getMethode() ?></td>
+       <td><?php echo $anlage->getRolleTm() ?></td>
+       <td><?php echo $anlage->getMaterial() ?></td>
+    </tr>	
+    <?php endforeach; ?>
+    <?php endforeach; ?>
     </tr>
   </tbody>
 </table>
