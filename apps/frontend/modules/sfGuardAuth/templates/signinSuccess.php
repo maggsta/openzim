63215741efb54ad3da6603065e8a4d7f49b82d0b
Ralph Magnus <info@magnus-network.de>
2010-07-08 15:56:52 +0200

    laguage update

---
+++ apps/frontend/modules/sfGuardAuth/templates/signinSuccess.php
@@ -0,0 +1,10 @@
+<?php use_helper('I18N') ?>
+
+<div class="signinform">
+<form action="<?php echo url_for('@sf_guard_signin') ?>" method="post">
+  <table>
+    <?php echo $form ?>
+  </table>
+  <input type="submit" value="" />
+</form>
+</div>
