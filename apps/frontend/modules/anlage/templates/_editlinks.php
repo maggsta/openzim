38e3a12dee90ab5bc45760ddf0f1a487b61e0e93
Christoph Herbst <christoph.p.herbst@gmx.de>
2010-07-11 13:14:05 +0200

    Anlage/editlinks: translation/added show link

--- apps/frontend/modules/anlage/templates/_editlinks.php
+++ apps/frontend/modules/anlage/templates/_editlinks.php
@@ -2,10 +2,11 @@
     <tfoot>
       <tr>
         <td colspan="2">
-          &nbsp;<?php echo link_to('Back to list','anlage') ?>
+          &nbsp;<?php echo link_to('Zurück zur Liste','anlage') ?>
+          &nbsp;<?php echo link_to('Vorschau','anlage_show',$form->getObject()) ?>
           &nbsp;<?php echo link_to('Export','anlage_export',$form->getObject()) ?>
           <?php if (!$form->getObject()->isNew()): ?>
-            &nbsp;<?php echo link_to('Delete', 'anlage_delete',$form->getObject(), array('method' => 'delete', 'confirm' => 'Are you sure?')) ?>
+            &nbsp;<?php echo link_to('Löschen', 'anlage_delete',$form->getObject(), array('method' => 'delete', 'confirm' => 'Are you sure?')) ?>
           <?php endif; ?>
           <input type="submit" value="Save" />
         </td>
