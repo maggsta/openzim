3e5a4f4131a58658c92de3646dad4b6072033d13
Christoph Herbst <christoph.p.herbst@gmx.de>
2010-07-14 00:27:02 +0200

    search.js: use 1s delay and use jscript namespace
    this prevents multiple searches while the user is typing

--- web/js/search.js
+++ web/js/search.js
@@ -1,37 +1,56 @@
-var doLoad = false;
-var oldValue;
+var openZIMsearch = {
 
-function doAjaxCall( value, action  ) {
-      //alert('got argument:' + value+ ':' + action );
-      $('#loader').show();
-      $('#anlagen').load(
-        action + ' #anlagen',
-        { query: value + '*' },
-        function() { $('#loader').hide(); }
-      );
-      doLoad = false;
+	doAjaxCall: function( value, action  ) {
+      		$('#loader').show();
+	      	$('#anlagen').load(
+	        	action + ' #anlagen',
+		        { query: value + '*' },
+		        function() {
+			   $('#loader').hide();
+			}
+	        );
+	        this.doLoad = false;
+	},
+	
+	cancel: function() {
+	    if(typeof this.timeoutID == "number") {
+	      window.clearTimeout(this.timeoutID);
+	      delete this.timeoutID;
+	    }
+	},
+
+	setup: function() {
+	  var self = this;
+	  $('#search_keywords').keyup(function(key)
+	  {
+	    // only react for backspace and delete
+	    if ( key.which != '8' && key.which != '46' && !self.doLoad )
+		return;
+	    if ( (this.value.length >= 3 || this.value == '' ) && 
+		   self.oldValue != this.value )
+	    {
+		// cancel pending ajax calls
+		self.cancel();
+		self.oldValue = this.value;
+		action = $(this).parents('form').attr('action');
+		// start new call with 1s delay
+		self.timeoutID = window.setTimeout(function(){
+					self.doAjaxCall(self.oldValue,action);
+				}, 1000);
+	    }
+	  });
+
+	  // fire for all keys except meta-keys
+	  $('#search_keywords').keypress(function(key)
+	  {
+	    if (this.value.length >= 2)
+	    	self.doLoad = true;
+	  });
+	}
 }
 
 $(document).ready(function()
 {
   //$('.search input[type="submit"]').hide();
- 
-  $('#search_keywords').keyup(function(key)
-  {
-    if ( key.which != '8' && key.which != '46' && !doLoad )
-	return;
-    if ( (this.value.length >= 3 || this.value == '' ) && 
-	   oldValue != this.value )
-    {
-	oldValue = this.value;
-	doAjaxCall(this.value,
-		$(this).parents('form').attr('action'));
-    }
-  });
-
-  $('#search_keywords').keypress(function(key)
-  {
-    if (this.value.length >= 2)
-    	doLoad = true;
-  });
+  openZIMsearch.setup();
 });
