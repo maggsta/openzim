ed11ee9cf05883734cfc8a998e9f658fe82e978f
magnus <magnus@magnus-network.de>
2010-07-14 15:15:58 +0000

    IE fixes

--- web/about.php
+++ web/about.php
@@ -63,6 +63,7 @@
 			<img src="/images/tiny.jpg" alt="tinymce">	
 		        <img src="/images/sqlite.png" alt="sqlite 3">				
 			<img src="/images/jquery.png" alt="jquery">
+			<img style="border:0;width:88px;height:31px" src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!" />
 	</div>
       </div>
  
