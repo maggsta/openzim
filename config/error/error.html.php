b722978608a7dd88f8397718bbcd8d8d81deecba
Ralph Magnus <info@magnus-network.de>
2010-07-15 17:08:41 +0200

    error500

---
+++ config/error/error.html.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <title>-= openZIM =-</title>
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="stylesheet" type="text/css" media="screen" href="/css/main.css" />
+  </head>
+  <body>
+    <div id="container">
+ 
+      <div id="header">
+	<h1>
+		<a href="/">
+          		<img src="/images/logo.jpg" alt="openZIM" />
+        	</a>
+	</h1>
+  	<div id="menu">
+    	<ul>
+		<li><a href="/">Startseite</a></li> 
+	</ul>
+  	</div>
+      </div>
+
+      <div id="content">
+        <div class="content">
+	<?php echo '<h1>ERROR:</h1>The server returned a 500 response.'; ?>
+	<br/><br/><p><a href="javascript: history.go(-1)">Back</a></p>
+	</div>
+      </div>
+ 
+      <div id="footer">
+        <div class="content">
+          <ul>
+	  </ul>
+	</div>
+      </div>
+    </div>
+  </body>
+</html>
