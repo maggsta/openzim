0ad167fb1b130f2aa805a2e7e0200f1dd96b513f
Christoph Herbst <christoph.p.herbst@gmx.de>
2010-07-15 15:22:01 +0200

    Paginator: refactored (DRY)

--- apps/frontend/modules/anlage/templates/indexSuccess.php
+++ apps/frontend/modules/anlage/templates/indexSuccess.php
@@ -10,37 +10,7 @@
 <div class="paginatelist" id="anlagen">
 
 <?php include_partial('anlage/list', array('anlagen' => $pager->getResults())) ?>
+<?php include_partial('global/paginator', array('pager' => $pager,
+						'internal_uri' => 'anlage/index')) ?>
 
-<?php if ($pager->haveToPaginate()): ?>
-  <div class="pagination">
-    <a href="<?php echo url_for('anlage/index') ?>?page=1">
-      <img src="/images/first.png" alt="First page" title="First page" />
-    </a>
- 
-    <a href="<?php echo url_for('anlage/index') ?>?page=<?php echo
-$pager->getPreviousPage() ?>">
-      <img src="/images/previous.png" alt="Previous page" title="Previous
-page" />
-    </a>
- 
-    <?php foreach ($pager->getLinks() as $page): ?>
-      <?php if ($page == $pager->getPage()): ?>
-        <?php echo $page ?>
-      <?php else: ?>
-        <a href="<?php echo url_for('anlage/index') ?>?page=<?php echo
-$page ?>"><?php echo $page ?></a>
-      <?php endif; ?>
-    <?php endforeach; ?>
- 
-    <a href="<?php echo url_for('anlage/index') ?>?page=<?php echo
-$pager->getNextPage() ?>">
-      <img src="/images/next.png" alt="Next page" title="Next page" />
-    </a>
- 
-    <a href="<?php echo url_for('anlage/index') ?>?page=<?php echo
-$pager->getLastPage() ?>">
-      <img src="/images/last.png" alt="Last page" title="Last page" />
-    </a>
-  </div>
-<?php endif; ?>
 </div>
--- apps/frontend/modules/zim/templates/indexSuccess.php
+++ apps/frontend/modules/zim/templates/indexSuccess.php
@@ -6,37 +6,6 @@
 <div class="paginatelist">
 
 <?php include_partial('zim/list', array('zims' => $pager->getResults())) ?>
-
-<?php if ($pager->haveToPaginate()): ?>
-  <div class="pagination">
-    <a href="<?php echo url_for('zim/index') ?>?page=1">
-      <img src="/images/first.png" alt="First page" title="First page" />
-    </a>
- 
-    <a href="<?php echo url_for('zim/index') ?>?page=<?php echo
-$pager->getPreviousPage() ?>">
-      <img src="/images/previous.png" alt="Previous page" title="Previous
-page" />
-    </a>
- 
-    <?php foreach ($pager->getLinks() as $page): ?>
-      <?php if ($page == $pager->getPage()): ?>
-        <?php echo $page ?>
-      <?php else: ?>
-        <a href="<?php echo url_for('zim/index') ?>?page=<?php echo
-$page ?>"><?php echo $page ?></a>
-      <?php endif; ?>
-    <?php endforeach; ?>
- 
-    <a href="<?php echo url_for('zim/index') ?>?page=<?php echo
-$pager->getNextPage() ?>">
-      <img src="/images/next.png" alt="Next page" title="Next page" />
-    </a>
- 
-    <a href="<?php echo url_for('zim/index') ?>?page=<?php echo
-$pager->getLastPage() ?>">
-      <img src="/images/last.png" alt="Last page" title="Last page" />
-    </a>
-  </div>
-<?php endif; ?>
+<?php include_partial('global/paginator', array('pager' => $pager,
+						'internal_uri' => 'zim/index')) ?>
 </div>
