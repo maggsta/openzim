111c27905ae45b1fccd807f5c29d3c7b437adaac
Christoph Herbst <christoph.p.herbst@gmx.de>
2010-07-23 00:28:22 +0200

    tinymce: add fullscreen plugin

--- web/js/tinymce.js
+++ web/js/tinymce.js
@@ -2,12 +2,16 @@ function initTinyMce () {
         tinyMCE.init({
         	mode : "textareas",
 	        theme : "advanced",
-		theme_advanced_buttons1 : "bold,italic,underline,separator,undo,redo,separator,cut,copy,pastetext,separator,newdocument,tinyautosave",
+		theme_advanced_buttons1 : "bold,italic,underline,|,undo,redo,|,cut,copy,pastetext,|,newdocument,tinyautosave,|,fullscreen",
 	        theme_advanced_buttons2 : "",
 	        theme_advanced_buttons3 : "",
-	        plugins: "autosave, tinyautosave, paste",
+	        plugins: "autosave, tinyautosave, paste,fullscreen",
 		paste_auto_cleanup_on_paste : true,
 	        entity_encoding : "raw",
+		theme_advanced_resizing : true,
+		theme_advanced_resize_horizontal : false,
+		theme_advanced_path : false,
+//		theme_advanced_statusbar_location : "bottom",
 		paste_preprocess : function(pl, o) {
         		o.content = o.content.replace(/&nbsp;/gi, ' '); 
 		},
