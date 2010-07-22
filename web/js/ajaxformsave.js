64861dbeab90f47b9d42cb091972878e2ec75ef0
Christoph Herbst <christoph.p.herbst@gmx.de>
2010-07-22 14:41:36 +0200

    ajaxformsave: handle ajax error response

--- web/js/ajaxformsave.js
+++ web/js/ajaxformsave.js
@@ -7,18 +7,28 @@
 var openZIMformsave = {
         autosave : 900000,
 	doSave: function() {
+	     var self = this;
       	     $('.form_loader').show();
       
 	      // do not do ajax call if there are any
 	      // non-empty file inputs
-	      if ( $("input:file[value|='']").length > 0 )
+	      if ( self.ajaxError || 
+		   $("input:file[value|='']").length > 0 )
 		return true;
-	
+
+	      self.ajaxError = false;
+
 	      tinyMCE.triggerSave(); 
 	      $('#form_data').load(
 	        $('#ajax_form').attr('action') + ' #form_data',
 	        $('#ajax_form').serializeArray(),
-	         function() { $('.form_loader').hide(); }
+	         function(response, status, xhr) {
+			if (status == "error") {
+				self.ajaxError = true;
+	  			$('#ajax_form').submit();
+			}else
+				$('.form_loader').hide(); 
+		 }
 	      );
 	      return false;
 	},
