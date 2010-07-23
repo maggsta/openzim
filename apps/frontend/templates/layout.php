4cc68e12d148b06a43ee63744b832d6d05f6abf6
Christoph Herbst <christoph.p.herbst@gmx.de>
2010-07-23 12:16:37 +0200

    template/database: add utf-8 specification

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -2,6 +2,7 @@
  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <title><?php include_slot('title', '-= openZIM =-') ?></title>
     <link rel="shortcut icon" href="/favicon.ico" />
     <?php use_javascript('jquery-1.4.2.min.js') ?>
