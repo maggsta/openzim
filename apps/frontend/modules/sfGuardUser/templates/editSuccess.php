1a1a7acdb0d2e7eda02f1fc8bb04b42a056abe55
Christoph Herbst <christoph.p.herbst@gmx.de>
2010-07-24 10:43:47 +0200

    sfGuardUser: update translation

--- apps/frontend/modules/sfGuardUser/templates/editSuccess.php
+++ apps/frontend/modules/sfGuardUser/templates/editSuccess.php
@@ -1,2 +1,2 @@
-<h1>Edit Anlage - <?php echo $form->getObject()->getUsername() ?></h1>
+<h1><?php echo __('Edit User').' - '. $form->getObject()->getUsername() ?></h1>
 <?php include_partial('form', array('form' => $form)) ?>
