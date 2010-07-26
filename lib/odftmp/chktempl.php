2dbbd53a326d43368cd956caeeca3958d6f93ccd
Christoph Herbst <christoph.p.herbst@gmx.de>
2010-07-26 09:33:34 +0200

    chktempl: added php script to extract styles from odt
    checks for common styles (bold,italic,underline)

---
+++ lib/odftmp/chktempl.php
@@ -0,0 +1,46 @@
+#!/usr/bin/php
+<?php
+
+if ( $argc < 2 ){
+	echo "Must give template argument!\n";;
+	exit(1);
+}
+
+$dir = '/tmp/chktmpl'. date('dmjgis');
+system('mkdir '.$dir);
+system('unzip '.$argv[1].' -d '.$dir.' >/dev/null');
+
+$contentXML = file_get_contents($dir.'/content.xml');
+//echo $contentXML;
+
+$reg = '#<style:style[^>]*>(.*)</style:style>#smU';
+preg_match_all($reg, $contentXML, $matches);
+
+$regs['bold'] = '#bold#';
+$regs['italic'] = '#italic#';
+$regs['underline'] = '#underline.*solid#';
+
+foreach( $regs as $key => $reg ){
+	$oldLen[$key] = strlen($contentXML);
+}
+
+foreach( $matches[0] as $match ){
+		
+	foreach( $regs as $key => $reg ){
+		if (preg_match($reg, $match, $matches2)) {
+			$len = strlen($match);
+			if ( $len < $oldLen[$key] ){
+				$oldLen[$key] = $len;
+				$bestMatch[$key] = $match;
+			}
+		}
+	}
+}
+foreach( $regs as $key => $reg ){
+	echo "Best style for $key:\n";
+	echo $bestMatch[$key]."\n";
+}
+
+
+system('rm -rf '.$dir);
+
