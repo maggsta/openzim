4049ef8892f5ceecce271c281bd1724e76d7193a
rb.magnus <rb.magnus@online.de>
2010-07-30 13:02:21 +0000

    install fix

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
+USERNAME=$1
+
+sudo chown -R ${USERNAME}:${USERNAME} cache log data web
 php symfony doctrine:insert-sql 
 php symfony doctrine:data-load --env=prod
 sudo chmod -R 777 log/ cache/
-sudo chown -R www-data:www-data cache
-sudo chown -R www-data:www-data log
-sudo chown -R www-data:www-data data
-sudo chown -R www-data:www-data web
+sudo chown -R www-data:www-data cache log data web
