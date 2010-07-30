a0a0c44511c5cdc908a1a72512f88d538c89642a
rb.magnus <rb.magnus@online.de>
2010-07-30 12:58:36 +0000

    install

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
-php symfony doctrine:build --all --and-load --env=prod
+php symfony doctrine:insert-sql 
+php symfony doctrine:data-load --env=prod
 sudo chmod -R 777 log/ cache/
+sudo chown -R www-data:www-data cache
+sudo chown -R www-data:www-data log
 sudo chown -R www-data:www-data data
 sudo chown -R www-data:www-data web
