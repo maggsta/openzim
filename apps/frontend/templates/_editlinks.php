5b76ada57631f0731c3a991d89734080c8f736e6
Christoph Herbst <christoph.p.herbst@gmx.de>
2010-08-03 07:12:01 +0200

    templates/_editlinks: do not show preview/export for new objects

--- apps/frontend/templates/_editlinks.php
+++ apps/frontend/templates/_editlinks.php
@@ -2,8 +2,12 @@
       <tr>
         <td colspan="2">
           &nbsp;<?php echo link_to(__('Back to List'),$name) ?>
-          &nbsp;<?php echo link_to(__('Preview'),$name.'_show',$object) ?>
-	  &nbsp;<?php echo link_to('Export',$name.'_export',$object) ?>
+          <?php if (!$object->isNew()): ?>
+	    &nbsp;<?php	echo link_to(__('Preview'),$name.'_show',$object) ?>
+          <?php endif; ?>
+          <?php if (!$object->isNew()): ?>
+	    &nbsp;<?php echo link_to('Export',$name.'_export',$object) ?>
+          <?php endif; ?>
           <?php if (!$object->isNew()): ?>
             &nbsp;<?php echo link_to(__('Delete'), $name.'_delete',$object, 
 			array('method' => 'delete', 'confirm' => __('Are you sure?'))) ?>
