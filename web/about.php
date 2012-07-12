13f3bec517925f79032b6e6fe7a66b82ae5fbfe0
Christoph Herbst <chris.p.herbst@googlemail.com>
2012-07-12 19:27:11 +0200

    web: add missing close slashes to about

--- web/about.php
+++ web/about.php
@@ -2,7 +2,7 @@
  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <title>-= openZIM =-</title>
     <link rel="shortcut icon" href="/favicon.ico" />
     <link rel="stylesheet" type="text/css" media="screen" href="/css/main.css" />
@@ -61,11 +61,11 @@
                 </table>
 		<h1>Credits</h1>
 	
-			<img src="/images/php.gif" alt="PHP5">
-			<img src="/images/symfony.gif" alt="Symfony 1.4">
-			<img src="/images/tiny.jpg" alt="tinymce">	
-		        <img src="/images/sqlite.png" alt="sqlite 3">				
-			<img src="/images/jquery.png" alt="jquery">
+			<img src="/images/php.gif" alt="PHP5" />
+			<img src="/images/symfony.gif" alt="Symfony 1.4" />
+			<img src="/images/tiny.jpg" alt="tinymce" />	
+		        <img src="/images/sqlite.png" alt="sqlite 3" />				
+			<img src="/images/jquery.png" alt="jquery" />
 			<img style="border:0;width:88px;height:31px" src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!" />
 			<img style="border:0;width:88px;height:31px" src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0!" />
 	</div>
