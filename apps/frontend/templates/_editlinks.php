3a85e64615e97dabe69ab15729c81b3b97d1702c
Christoph Herbst <chris.p.herbst@googlemail.com>
2012-07-14 13:05:17 +0200

    openzim: simplify editlinks template

--- apps/frontend/templates/_editlinks.php
+++ apps/frontend/templates/_editlinks.php
@@ -2,14 +2,10 @@
       <tr>
         <td colspan="2">
           &nbsp;<?php echo link_to(__('Back to List'),$name) ?>
-          <?php if (!$object->isNew()): ?>
-	    &nbsp;<?php	echo link_to(__('Preview'),$name.'_show',$object) ?>
-          <?php endif; ?>
-          <?php if (!$object->isNew()): ?>
-	    &nbsp;<?php echo link_to('Export',$name.'_export',$object) ?>
-          <?php endif; ?>
-          <?php if (!$object->isNew()): ?>
-            &nbsp;<?php echo link_to(__('Delete'), $name.'_delete',$object, 
+          <?php if (!$object->isNew()): ?>&nbsp;
+          <?php	echo link_to(__('Preview'),$name.'_show',$object) ?>&nbsp;
+          <?php echo link_to('Export',$name.'_export',$object) ?>&nbsp;
+          <?php echo link_to(__('Delete'), $name.'_delete',$object, 
 			array('method' => 'delete', 'confirm' => __('Are you sure?'))) ?>
           <?php endif; ?>
           <input type="submit" value="<?php echo __('Save') ?>" />
