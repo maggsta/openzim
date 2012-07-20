b5797dc80601bc989aaea3434ee1f022bf931439
magnus <info@magnus-network.de>
2012-07-20 12:27:41 +0200

    Filter_Search_Bar: refined css and graphics

--- apps/frontend/modules/anlage/templates/_search.php
+++ apps/frontend/modules/anlage/templates/_search.php
@@ -3,7 +3,7 @@
    <form action="<?php echo url_for('anlage_search') ?>" method="get">
    				<?php echo $form ?>
                 <input type="text" name="query" value="<?php echo $sf_request->getParameter('query') ?>" id="search_keywords" />
-                <input type="submit" value="GO" />
+                <input type="submit" id="search_submit" value="GO" />
                 <img id="loader" src="/images/loader.gif" alt="search_loader" style="vertical-align: middle; display: none" />
                 <div class="help">
                         Suche nach Keywords (Name, Ziel, Inhalt, ...)
